04c1cb4875681f246ae0525337bb78697e3338d9
princian123 <prince.pankaj994@gmail.com>
2025-04-06 02:08:26 +0530

    initial commit

---
+++ first.js
@@ -0,0 +1,13 @@
+let car = [];
+car.push("BMW");
+car.push("Mercedes");
+car.push("Audi");
+for(var i=0; i < car.length; i++){
+    console.log(car[i]);
+};
+
+const car1 = (name)=>
+{
+ console.log("This is a car function :"+" "+ name);
+}
+car1("BMW");
